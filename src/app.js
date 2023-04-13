10877bf9c62819818334adea3e0bca043f3bdba2
larissa <larissaalves1102@gmail.com>
2023-04-13 17:39:14 -0300

    Requisito POST/participants sem o uso da biblioteca joi

--- src/app.js
+++ src/app.js
@@ -1,8 +1,48 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import dayjs from 'dayjs';
 
-const PORT = 5000;
 const app = express();
 app.use(cors());
-app.use(json());
-app.listen(PORT);
+app.use(express.json());
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+mongoClient.connect()
+    .then(() => db = mongoClient.db())
+    .catch((err) => console.log(err.message));
+
+
+app.post("/participants", (req, res) => {
+    const { name } = req.body;
+
+    if (!name || typeof (name) !== "string") {
+        return res.sendStatus(422);
+    }
+    const lastStatus = Date.now();
+    const newParticipant = { name, lastStatus };
+    const time = dayjs(lastStatus).format("HH:mm:ss");
+    const newMessage = { from: name, to: "Todos", text: "entra na sala...", type: "status", time: time }
+
+    db.collection("participants").findOne({ name: name })
+        .then((data) => {
+            if (data) {
+                return res.sendStatus(409);
+            } else {
+                db.collection("participants").insertOne(newParticipant)
+                    .then(() => {
+                        db.collection("messages").insertOne(newMessage)
+                            .then(res.sendStatus(201))
+                            .catch(err => res.status(500).send(err.message))
+                    })
+                    .catch(err => res.status(500).send(err.message))
+            }
+        })
+        .catch(err => res.status(500).send(err.message))
+})
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
